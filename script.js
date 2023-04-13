2275cdbdfde931689bb707600c4db1c215d8d6ba
SwapnilYC <119727525+SwapnilYC@users.noreply.github.com>
2023-04-13 18:30:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,30 +1,37 @@
 //complete this code
 // parent class is person
-class Person {
-    constructor(name, age) {
-        this.name = name;
-        this.age = age;
-    }
-    get getterForName() {
-        return this.name;
-    }
 
-    set setterForAge(age) {
-        this.age = age;
-    }
+class Person {
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+  }
+  get getterForName() {
+    return this.name;
+  }
+
+  set setterForAge(age) {
+    this.age = age;
+  }
 }
 
 // student and teacher are subclasses/child classes
 class Student extends Person {
-    study() {
-        console.log(`${super.name} is studying.`);
-    }
+  constructor(name, age) {
+    super(name, age);
+  }
+  study() {
+    console.log(`${this.name} is studying.`);
+  };
 }
 
 class Teacher extends Person {
-    teach() {
-        console.log(`${super.name} is teaching.`);
-    }
+  constructor(name, age) {
+    super(name, age);
+  }
+  teach() {
+    console.log(`${this.name} is teaching.`);
+  };
 }
 
 // Do not change the code below this line
